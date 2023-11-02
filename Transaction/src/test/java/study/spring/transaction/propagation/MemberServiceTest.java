770b04412d41f703b23b7544cbf9ef671188e5fd
Daeeun Ko <7273322@naver.com>
2023-11-02 23:06:53 +0900

    트랜잭션 전파 활용 1 - 외부 트랜잭션 OFF, 내부 트랜잭션 ON

---
+++ Transaction/src/test/java/study/spring/transaction/propagation/MemberServiceTest.java
@@ -0,0 +1,34 @@
+package study.spring.transaction.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired LogRepository logRepository;
+
+    /**
+     * memberService     @Transactional: OFF
+     * memberRepository  @Transactional: ON
+     * logRepository     @Transactional: ON
+     */
+    @Test
+    void outerTxOff_success() {
+        String username = "outerTxOff_success";
+
+        memberService.joinV1(username);
+
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+}
